b662da4407cfd7a98f889cbbeed413931d8e5664
Holly Tyler <hollystertyler@gmail.com>
2023-07-26 09:19:41 +0100

    more work on part 1

--- task_1.py
+++ task_1.py
@@ -3,22 +3,22 @@ class CardGame:
 
 # In the first function the first mistake would be the =' is meant to be '=='
 # The second mistake is the syntax mistake of a missing ':' on the 'else'
-  # def check_for_ace(self, card):
-  #   if card.value = 1:
-  #     return True
-  #   else
-  #     return False
+  def check_for_ace(self, card):
+    if card.value = 1:
+      return True
+    else
+      return False
 
 
 # In the second function the first mistake is the spelling mistake 'dif' should be 'def'
 # - without 'def' the function is not defined
 # The second mistake is in the first return its asking to return 'card' but card doesnt exist in this function
 # - the return should be asking for 'card1' otherwise it will not know what to return
-  # dif highest_card(self, card1 card2):
-  # if card1.value > card2.value:
-  #   return card
-  # else:
-  #   return card2
+  dif highest_card(self, card1 card2):
+  if card1.value > card2.value:
+    return card
+  else:
+    return card2
   
 
 # In the third function, the first mistake is an indentation error. 
